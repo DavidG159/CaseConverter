fee9ee8264114539e75b666289f1afda3b26cf77
DavidG159 <gaspardavid235@gmail.com>
2025-07-29 21:21:02 -0600

    added js2

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 //variable
 const lowercase = document.querySelector('lowCase');
+const uppercase = document.querySelector('uppderCase');
+const mixedcase = document.querySelector('mixedCase');
+
 
 //function
 
